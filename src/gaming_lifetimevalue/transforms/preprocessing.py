9ca6b9ee2a6b0977467573d69b3dcd0a5c0f1e45
bpugnaire <baptiste.pugnaire@gmail.com>
2025-12-28 00:21:35 +0100

    fix/fixers

--- src/gaming_lifetimevalue/transforms/preprocessing.py
+++ src/gaming_lifetimevalue/transforms/preprocessing.py
@@ -1,5 +1,6 @@
 import polars as pl
 
+
 def add_segmentation_cohorts(pl_df: pl.DataFrame) -> pl.DataFrame:
     """
     Add segmentation cohorts to the DataFrame based on revenue quantiles.
@@ -12,27 +13,33 @@ def add_segmentation_cohorts(pl_df: pl.DataFrame) -> pl.DataFrame:
     no_rev_users = pl_df.filter(pl.col("d120_rev") == 0)
     rev_users = pl_df.filter(pl.col("d120_rev") > 0)
     rev_users_segmented = rev_users.with_columns(
-        top_rev_1pct = pl.col("d120_rev") >= pl.col("d120_rev").quantile(0.99),
-        top_rev_5pct = pl.col("d120_rev") >= pl.col("d120_rev").quantile(0.95),
-        top_rev_20pct = pl.col("d120_rev") >= pl.col("d120_rev").quantile(0.80),
-        top_rev_50pct = pl.col("d120_rev") >= pl.col("d120_rev").quantile(0.50),
+        top_rev_1pct=pl.col("d120_rev") >= pl.col("d120_rev").quantile(0.99),
+        top_rev_5pct=pl.col("d120_rev") >= pl.col("d120_rev").quantile(0.95),
+        top_rev_20pct=pl.col("d120_rev") >= pl.col("d120_rev").quantile(0.80),
+        top_rev_50pct=pl.col("d120_rev") >= pl.col("d120_rev").quantile(0.50),
     )
     rev_with_cohorts = rev_users_segmented.with_columns(
-        cohort = pl.when(pl.col("top_rev_1pct")).then(pl.lit("Top 1%"))
-                .when(pl.col("top_rev_5pct")).then(pl.lit("Top 5%"))
-                .when(pl.col("top_rev_20pct")).then(pl.lit("Top 20%"))
-                .when(pl.col("top_rev_50pct")).then(pl.lit("Top 50%"))
-                .otherwise(pl.lit("Others"))
+        cohort=pl.when(pl.col("top_rev_1pct"))
+        .then(pl.lit("Top 1%"))
+        .when(pl.col("top_rev_5pct"))
+        .then(pl.lit("Top 5%"))
+        .when(pl.col("top_rev_20pct"))
+        .then(pl.lit("Top 20%"))
+        .when(pl.col("top_rev_50pct"))
+        .then(pl.lit("Top 50%"))
+        .otherwise(pl.lit("Others"))
     )
-    no_rev_with_cohorts = no_rev_users.with_columns(
-        cohort = pl.lit("No Revenue")
+    no_rev_with_cohorts = no_rev_users.with_columns(cohort=pl.lit("No Revenue"))
+    rev_with_cohorts = rev_with_cohorts.drop(
+        ["top_rev_1pct", "top_rev_5pct", "top_rev_20pct", "top_rev_50pct"]
     )
-    rev_with_cohorts = rev_with_cohorts.drop(['top_rev_1pct', 'top_rev_5pct', 'top_rev_20pct', "top_rev_50pct"])
 
     return pl.concat([no_rev_with_cohorts, rev_with_cohorts])
 
 
-def remove_columns_per_horizons(pl_df: pl.DataFrame, horizons: list[int]) -> pl.DataFrame:
+def remove_columns_per_horizons(
+    pl_df: pl.DataFrame, horizons: list[int]
+) -> pl.DataFrame:
     """
     Remove all columns from the DataFrame that are associated with the specified horizons.
     Args:
@@ -42,11 +49,16 @@ def remove_columns_per_horizons(pl_df: pl.DataFrame, horizons: list[int]) -> pl.
     Returns:
         pl.DataFrame: DataFrame with specified horizons columns removed.
     """
-    horizons_cols = [col for col in pl_df.columns if any(horizon in col for horizon in horizons)]
+    horizons_cols = [
+        col for col in pl_df.columns if any(horizon in col for horizon in horizons)
+    ]
     existing_cols_to_remove = [col for col in horizons_cols if col in pl_df.columns]
     return pl_df.drop(existing_cols_to_remove)
 
-def remove_redundant_columns(pl_df: pl.DataFrame, redundant_cols: list[str]) -> pl.DataFrame:
+
+def remove_redundant_columns(
+    pl_df: pl.DataFrame, redundant_cols: list[str]
+) -> pl.DataFrame:
     """
     Remove redundant columns from the DataFrame.
     Args:
@@ -59,7 +71,10 @@ def remove_redundant_columns(pl_df: pl.DataFrame, redundant_cols: list[str]) -> 
     existing_cols_to_remove = [col for col in redundant_cols if col in pl_df.columns]
     return pl_df.drop(existing_cols_to_remove)
 
-def bin_high_cardinality(pl_df: pl.DataFrame, column: str, top_n: int = 10) -> pl.DataFrame:
+
+def bin_high_cardinality(
+    pl_df: pl.DataFrame, column: str, top_n: int = 10
+) -> pl.DataFrame:
     """
     Bin high-cardinality categorical variable into top N categories and 'Other'.
     Args:
@@ -88,7 +103,8 @@ def bin_high_cardinality(pl_df: pl.DataFrame, column: str, top_n: int = 10) -> p
         pl.col(column).cast(pl.Categorical).to_physical().alias(column)
     )
 
-def process_install_date(pl_df: pl.DataFrame, date_col: str ) -> pl.DataFrame:
+
+def process_install_date(pl_df: pl.DataFrame, date_col: str) -> pl.DataFrame:
     """
     Process install date column to extract useful features.
     Args:
@@ -98,14 +114,14 @@ def process_install_date(pl_df: pl.DataFrame, date_col: str ) -> pl.DataFrame:
     Returns:
         pl.DataFrame: DataFrame with processed install date features.
     """
-    return (
-        pl_df.with_columns([
+    return pl_df.with_columns(
+        [
             pl.col(date_col).dt.year().alias(f"{date_col}_year"),
             pl.col(date_col).dt.month().alias(f"{date_col}_month"),
             pl.col(date_col).dt.weekday().alias(f"{date_col}_dow"),
-        ])
-        .drop(date_col)
-    )
+        ]
+    ).drop(date_col)
+
 
 def convert_to_categorical(pl_df: pl.DataFrame, cat_cols: list) -> pl.DataFrame:
     """
@@ -117,7 +133,9 @@ def convert_to_categorical(pl_df: pl.DataFrame, cat_cols: list) -> pl.DataFrame:
         pl.DataFrame: DataFrame with specified columns converted to categorical.
     """
     for col in cat_cols:
-        pl_df = pl_df.with_columns(pl.col(col).cast(pl.Categorical).to_physical().alias(col))
+        pl_df = pl_df.with_columns(
+            pl.col(col).cast(pl.Categorical).to_physical().alias(col)
+        )
     return pl_df
 
 
@@ -130,23 +148,32 @@ def add_gaming_velocity_features(pl_df: pl.DataFrame) -> pl.DataFrame:
     Returns:
         pl.DataFrame: DataFrame with added gaming velocity features.
     """
-    return pl_df.with_columns([
-        (pl.col("d0_rev") / pl.col("session_count_d0").replace(0, 1))
-        .alias("rev_per_session"),
-        
-        (pl.col("game_count_d0") / pl.col("session_count_d0").replace(0, 1))
-        .alias("games_per_session"),
-        
-        (pl.col("session_length_d0") / pl.col("session_count_d0").replace(0, 1))
-        .alias("avg_session_duration"),
-
-        (pl.col("coins_spend_sum_d0") / pl.col("session_count_d0").replace(0, 1))
-        .alias("coins_spent_per_session"),
-        
-        (pl.col("rv_shown_count_d0") / pl.col("game_count_d0").replace(0, 1))
-        .alias("rv_per_game"),
-        
-        (pl.col("current_level_d0") / pl.col("session_length_d0").replace(0, 1))
-        .alias("level_velocity"),
-        
-    ]).fill_nan(0).fill_null(0)
+    return (
+        pl_df.with_columns(
+            [
+                (pl.col("d0_rev") / pl.col("session_count_d0").replace(0, 1)).alias(
+                    "rev_per_session"
+                ),
+                (
+                    pl.col("game_count_d0") / pl.col("session_count_d0").replace(0, 1)
+                ).alias("games_per_session"),
+                (
+                    pl.col("session_length_d0")
+                    / pl.col("session_count_d0").replace(0, 1)
+                ).alias("avg_session_duration"),
+                (
+                    pl.col("coins_spend_sum_d0")
+                    / pl.col("session_count_d0").replace(0, 1)
+                ).alias("coins_spent_per_session"),
+                (
+                    pl.col("rv_shown_count_d0") / pl.col("game_count_d0").replace(0, 1)
+                ).alias("rv_per_game"),
+                (
+                    pl.col("current_level_d0")
+                    / pl.col("session_length_d0").replace(0, 1)
+                ).alias("level_velocity"),
+            ]
+        )
+        .fill_nan(0)
+        .fill_null(0)
+    )
